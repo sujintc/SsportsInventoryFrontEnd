24308fcaf295ddfbc1d4acc297c1d288950b4f5e
sujintc <133184229+sujintc@users.noreply.github.com>
2024-01-05 08:27:32 +0530

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -11,7 +11,7 @@
 
 //   // const handleLogin = async () => {
 //   //   try {
-//   //     const response = await fetch('http://localhost:4001/api/login', {
+//   //     const response = await fetch(https://ssportsstorebackend.onrender.com/api/login', {
 //   //       method: 'POST',
 //   //       headers: {
 //   //         'Content-Type': 'application/json',
@@ -112,7 +112,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:4001/api/login', {
+      const response = await fetch('https://ssportsstorebackend.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
